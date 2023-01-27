dd80a6a0e00d3909bf8299254b54f4dd7e77c504
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-27 08:37:06 +0600

    [ADD] boolean type variable

--- variable.js
+++ variable.js
@@ -1,7 +1,48 @@
 // vary + able = variable.
+
+// Number / Numeric type variable
 var price = 13;
 var age = 21;
 var temperature = 37;
-var name = "John";
+
+
+// String type variable
+var name = "Himel";
+var address = "Dhaka";
+var country = "Bangladesh";
+var location = "Dhaka, Bangladesh";
+var programmingLanguage = "JavaScript";
+var actress = "Scarlett Johansson";
+var actro = "Robert Downey Jr.";
+var movie = "Avengers: Endgame";
+
+
+
+// Boolean type variable
 var isRaining = true;
 var isSunny = false;
+var serious = true;
+var isFullMark = false;
+var isSingle = true;
+var isMarried = false;
+var isHappy = true;
+var isSad = false;
+var isRich = true;
+var isPoor = false;
+var isGood = true;
+var isBad = false;
+var isHot = true;
+var isCold = false;
+var isTall = true;
+var isShort = false;
+var isFat = true;
+var isThin = false;
+var isSmart = true;
+var isDumb = false;
+var isHandsome = true;
+var isBeautiful = true;
+var isUgly = false;
+var isCute = true;
+var isFunny = true;
+
+
