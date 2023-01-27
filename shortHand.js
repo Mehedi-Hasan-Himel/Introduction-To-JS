0a9543241e1c0c00b1792bf043e7bf5583b45fe8
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-27 09:19:21 +0600

    [ADD] shortHand

---
+++ shortHand.js
@@ -0,0 +1,48 @@
+// variable Declearation
+var tomatoPrice = 25;
+
+// variable value update
+tomatoPrice = 30;
+
+// add something to previous value
+var price1 = 31;
+var price2 = 10;
+
+// set a new value to a variable
+// var price1 = 35
+
+// add some value to existing value
+price1 = price1 + 10;
+console.log(price1);
+
+// addition to the same variable shortHand
+// += means adding some value to the existing value
+price1 += 10;
+
+price2 = price2 - 5;
+// -= means subtracting some value from the existing value
+price2 -= 5;
+
+console.log(price2);
+
+
+// multiplication
+price1 *= 5;
+
+
+// division
+price1 /= 5;
+
+
+
+var age = 15;
+age = age + 1;
+age++  // ++ means adding 1 to the existing value
+console.log(age);
+
+
+var love = 100;
+love = love - 1;
+// -- means subtracting 1 from the existing value
+love--;
+console.log(love);
