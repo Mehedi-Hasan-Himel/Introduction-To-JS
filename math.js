f4c4d5472dd2ef89c520e63c893334ce4df7ca3d
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-27 08:28:01 +0600

    [ADD] Variable

---
+++ math.js
@@ -0,0 +1,6 @@
+/*
+1. number ==> 11,555,700;
+2. intiger ==> 2,5;
+3. float : decimal ==> 2.5, 10.5;
+4. operation ==> addition, substraction, multiplication, division, reminder, persentsge, average, comparison (< , > , ==, === ), ;
+*/ 
---
+++ variable.js
@@ -0,0 +1,7 @@
+// vary + able = variable.
+var price = 13;
+var age = 21;
+var temperature = 37;
+var name = "John";
+var isRaining = true;
+var isSunny = false;
