19994f562b12a4016d389a52f2de951dba62e98c
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-27 10:26:07 +0600

    [ADD] String Concatenation, type conversion parseInt parseFloat

---
+++ conversion.js
@@ -0,0 +1,23 @@
+// var price1 = 31;
+// var price2 = 10;
+// var total = price1 + price2;
+// console.log(total);
+
+
+var firstName = 'Jennifer';
+var lastName = 'Kim';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+var price1 = "31"; 
+var price2 = "10";
+var total = price1 + price2;
+
+var priceNumber = parseInt(price1)
+
+console.log(priceNumber);
+
+
+var gpa = "3.41"
+var gpaNumber = parseFloat(gpa) 
+console.log(gpaNumber);
