d9a332102ebfe26014a4eb130c39d52441bbd74a
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-27 09:02:29 +0600

    [ADD] operator

---
+++ operation.js
@@ -0,0 +1,33 @@
+var onionPrice = 100;
+var eggPrice = 10;
+
+// addition + substraction
+var totalPrice = onionPrice + eggPrice;
+var priceDeferece = onionPrice - eggPrice;
+
+
+console.log('====================================');
+console.log(onionPrice);
+console.log(eggPrice);
+// console.log(onionPrice + eggPrice);
+// console.log(onionPrice - eggPrice);
+console.log(totalPrice);
+console.log(priceDeferece);
+console.log('====================================');
+
+// multiplication
+var orangePrice = 20;
+var quantity = 7;
+
+var totalCost = orangePrice * quantity;
+
+console.log(totalCost);
+
+// division
+
+var moneyAvailable = 500;
+var player = 10;
+
+var eachPlayerCost = moneyAvailable / player;
+
+console.log(eachPlayerCost);
