d14ba5f7c34f0eb9eb42bed491fc174e873b36e6
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-28 10:36:12 +0600

    [ADD] Problem 01

---
+++ problem1.js
@@ -0,0 +1,16 @@
+//  total money
+var momMoney = 1000;
+
+console.log("Mother gives money : " + momMoney);
+
+// product price
+var applePrice = 400;
+var orangePrice = 300;
+var totalPrice = applePrice + orangePrice;
+
+console.log("Total Price: " + totalPrice);
+
+// Return money
+
+var returnMoney = momMoney - totalPrice;
+console.log("Return Money: " + returnMoney);
