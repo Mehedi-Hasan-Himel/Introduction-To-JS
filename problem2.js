fe03092a476fce8d9272fdb87802ee39686f7ca0
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-28 10:41:37 +0600

    [ADD] Problem 2

---
+++ problem2.js
@@ -0,0 +1,14 @@
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.5;
+
+
+var totalMarks = math + biology + chemistry + physics + bangla;
+
+console.log("Total Marks: " + totalMarks);
+
+
+var avarage = (totalMarks / 5).toFixed(2);
+console.log("Avarage Marks: " + avarage);
